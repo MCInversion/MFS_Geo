c763746341758fe4ad52d55e3b90235677fa4154
MCInversion <mcavarga@gmail.com>
2019-03-28 15:14:20 +0100

    fixing input data, writing data file

--- MFS_Geo/MFS_Geo.cpp
+++ MFS_Geo/MFS_Geo.cpp
@@ -4,25 +4,42 @@
 #include "pch.h"
 
 // global constants
-double R = 6371.; // Earth radius [km]
-double r_remaining = 11; // [km]
-double GM = 389600.5; // [km^3 * s^(-1)]
-double uExact = GM / R; // exact surface potential
-double qExact = GM / (R * R); // surface acceleration
+double R = 6378000.; // Earth radius [m]
+double r_remaining = 500000; // [m]
+//double GM = 389600.5; // [km^3 * s^(-1)]
+//double uExact = GM / R; // exact surface potential
+//double qExact = GM / (R * R); // surface acceleration
 
-int N = 902; // dataset size;
+// const char* dataFilename = "BL-902.dat";
+// const char* dataFilename = "BL-1298.dat";
+// const char* dataFilename = "BL-3602.dat";
+const char* dataFilename = "BL-8102.dat";
+int N = 8102; // dataset size;
 
-void printArray1 (std::string name, double *a, int printLim) {
-	std::string offset = std::string((name + " = ").length() + 1, ' ');
-	std::cout << name << " = " << std::endl;
-	for (int i = 0; i < printLim; i++) {
-		std::cout << offset << a[i] << std::endl;
+void printArray1 (std::string name, double *a, int printLim, bool inRow = true) {
+	if (inRow) {
+		std::cout << name << " = " << std::endl;
+		for (int i = 0; i < printLim; i++) {
+			std::cout << " " << a[i];
+		}
+		std::cout << "  ... ";
+		for (int i = N - printLim; i < N; i++) {
+			std::cout << " " << a[i];
+		}
+		std::cout << std::endl;
 	}
-	for (int i = 0; i < 3; i++) std::cout << offset << "  ." << std::endl;
-	for (int i = N - printLim; i < N; i++) {
-		std::cout << offset << a[i] << std::endl;
+	else {
+		std::string offset = std::string((name + " = ").length() + 1, ' ');
+		std::cout << name << " = " << std::endl;
+		for (int i = 0; i < printLim; i++) {
+			std::cout << offset << a[i] << std::endl;
+		}
+		for (int i = 0; i < 3; i++) std::cout << offset << "  ." << std::endl;
+		for (int i = N - printLim; i < N; i++) {
+			std::cout << offset << a[i] << std::endl;
+		}
+		std::cout << std::endl;
 	}
-	std::cout << std::endl;
 }
 
 void printArrayVector3 (std::string name, double *vx, double *vy, double *vz, int printLim) {
@@ -67,17 +84,22 @@ void printArray2 (std::string name, double **A, int printLim) {
 	std::cout << std::endl;
 }
 
-void loadPointData (double *x, double *y, double *z, double *sx, double *sy, double *sz, double *q, double *d2U) {
+void loadPointData (
+	double *B, double *L, double *H,
+	double *x, double *y, double *z,
+	double *sx, double *sy, double *sz,
+	double *q, double *d2U
+) {
 	printf("Loading data ... \n");
 
 	std::fstream dataFile;
-	dataFile.open("BL-902.dat", std::fstream::in);
+	dataFile.open(dataFilename, std::fstream::in);
 
 	if (!dataFile.is_open()) {
-		printf("Unable to open file BL-902.dat\n");
+		printf("Unable to open file %s\n", dataFilename);
 	}
 	else {
-		printf("BL-902.dat opened successfully\n");
+		printf("%s opened successfully\n", dataFilename);
 
 		std::string line;
 		int i = 0;
@@ -92,21 +114,21 @@ void loadPointData (double *x, double *y, double *z, double *sx, double *sy, dou
 				line = line.erase(0, pos + s_delimiter.length());
 			}
 
-			double B = std::stod(tokens[0]);
-			double L = std::stod(tokens[1]);
-			double H = std::stod(tokens[2]);
+			B[i] = std::stod(tokens[0]);
+			L[i] = std::stod(tokens[1]);
+			H[i] = std::stod(tokens[2]);
 			double Q = std::stod(tokens[3]);
 			double D2U = std::stod(tokens[4]);
 
-			sx[i] = R * cos(B) * cos(L);
-			sy[i] = R * cos(B) * sin(L);
-			sz[i] = R * sin(B);
+			sx[i] = (R - r_remaining) * cos(B[i] * M_PI / 180) * cos(L[i] * M_PI / 180);
+			sy[i] = (R - r_remaining) * cos(B[i] * M_PI / 180) * sin(L[i] * M_PI / 180);
+			sz[i] = (R - r_remaining) * sin(B[i] * M_PI / 180);
 
-			x[i] = (R + 0.001 * H + r_remaining) * cos(B) * cos(L);
-			y[i] = (R + 0.001 * H + r_remaining) * cos(B) * sin(L);
-			z[i] = (R + 0.001 * H + r_remaining) * sin(B);
+			x[i] = (R + H[i]) * cos(B[i] * M_PI / 180) * cos(L[i] * M_PI / 180);
+			y[i] = (R + H[i]) * cos(B[i] * M_PI / 180) * sin(L[i] * M_PI / 180);
+			z[i] = (R + H[i]) * sin(B[i] * M_PI / 180);
 
-			q[i] = Q;
+			q[i] = 0.00001 * Q;
 			d2U[i] = D2U;
 
 			// std::cout << B << " " << L << " " << H << " " << Q << " " << D2U << std::endl;
@@ -155,7 +177,7 @@ double vectorDot (double *a, double *b) {
 	return result;
 }
 
-double vectorNorm(double *a) {
+double vectorNorm (double *a) {
 	return sqrt(vectorDot(a,a));
 }
 
@@ -187,7 +209,7 @@ void copyVector(double *original, double *target) {
 void Bi_CGSTAB_solve (double **A, double *b, double *x) {
 	// ctrl. constants
 	int maxIter = 100;
-	double tol = 1E-6;
+	double tol = 2e-3;
 
 	// iter vectors
 	double *x_curr = new double[N];
@@ -241,10 +263,12 @@ void Bi_CGSTAB_solve (double **A, double *b, double *x) {
 		smultVector(alpha, tmp, tmp);
 		subVectors(r_curr, tmp, s);
 
+		std::cout << "||s|| = " << vectorNorm(s) << std::endl;
 		if (vectorNorm(s) < tol) {
 			// x[k + 1] = x[k] + alpha[k] * p[k]
 			smultVector(alpha, p_curr, tmp);
 			addVectors(x_curr, tmp, x_next);
+			std::cout << "||s|| < tol = " << tol << ", exiting iterations" << std::endl;
 			break;
 		}
 
@@ -256,12 +280,14 @@ void Bi_CGSTAB_solve (double **A, double *b, double *x) {
 		addVectors(x_curr, tmp, tmp);
 		smultVector(omega, s, tmp1);
 		addVectors(tmp, tmp1, x_next);
-		printArray1("x", x_next, 5);
+		// printArray1("x", x_next, 5);
 		// r[k + 1] = s[k] - omega[k] * A s[k]
 		mmultVector(A, s, tmp);
 		smultVector(omega, tmp, tmp);
 		subVectors(s, tmp, r_next);
+		std::cout << "||r[k + 1]|| = " << vectorNorm(r_next) << std::endl;
 		if (vectorNorm(r_next) < tol) {
+			std::cout << "||r[k + 1]|| < tol = " << tol << ", exiting iterations" << std::endl;
 			break;
 		}
 		// beta[k] = (alpha[k] / omega[k]) * <r[k + 1], rp0> / <r[k], rp0>
@@ -272,8 +298,10 @@ void Bi_CGSTAB_solve (double **A, double *b, double *x) {
 		subVectors(p_curr, tmp, tmp);
 		smultVector(beta, tmp, tmp);
 		addVectors(r_next, tmp, p_next);
+		std::cout << "|< r[k + 1], rp0 >| = " << fabs(vectorDot(r_next, rp0)) << std::endl;
 		if (fabs(vectorDot(r_next, rp0)) < tol) {
 			// rp0 = r[k + 1]; p[k + 1] = r[k + 1]
+			// std::cout << "|< r[k + 1], rp0 >| < tol = " << tol << ", copying r[k + 1] to rp0 and p[k + 1]" << std::endl;
 			copyVector(r_next, rp0);
 			copyVector(r_next, p_next);
 		}
@@ -293,7 +321,23 @@ void Bi_CGSTAB_solve (double **A, double *b, double *x) {
 	delete[] s; delete[] tmp; delete[] tmp1;
 }
 
+
+void writeData(double *B, double *L, double *u) {
+	std::fstream dataOut;
+	dataOut.open("data.dat", std::fstream::out);
+
+	for (int i = 0; i < N; i++) {
+		dataOut << B[i] << " " << L[i] << " " << u[i] << std::endl;
+	}
+
+	dataOut.close();
+}
+
 int main() {
+	double *B = new double[N];
+	double *L = new double[N];
+	double *H = new double[N];
+
 	double *x = new double[N];
 	double *y = new double[N];
 	double *z = new double[N];
@@ -305,7 +349,7 @@ int main() {
 	double *q = new double[N];
 	double *d2U = new double[N];
 
-	loadPointData(x, y, z, sx, sy, sz, q, d2U);
+	loadPointData(B, L, H, x, y, z, sx, sy, sz, q, d2U);
 
 	printArrayVector3("x", x, y, z, 5);
 	printArrayVector3("s", sx, sy, sz, 5);
@@ -338,6 +382,8 @@ int main() {
 	mmultVector(G, alphas, u);
 
 	printArray1("u", u, 8);
+
+	writeData(B, L, u);
 
 	// clean up
 	delete[] x; delete[] y; delete[] z;
