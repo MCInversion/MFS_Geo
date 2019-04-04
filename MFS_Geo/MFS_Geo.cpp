b71332f40cd39b0f427c4d5bca860416110cc471
MCavarga <mcavarga@gmail.com>
2019-04-04 11:39:37 +0200

    migration of Bi-CGSTAB to main complete

--- MFS_Geo/MFS_Geo.cpp
+++ MFS_Geo/MFS_Geo.cpp
@@ -8,10 +8,10 @@ double r_remaining = 500000; // [m]
 //double qExact = GM / (R * R); // surface acceleration
 
 // const char* dataFilename = "BL-902.dat";
-// const char* dataFilename = "BL-1298.dat";
-const char* dataFilename = "BL-3602.dat";
+const char* dataFilename = "BL-1298.dat";
+// const char* dataFilename = "BL-3602.dat";
 // const char* dataFilename = "BL-8102.dat";
-int N = 3602; // dataset size;
+int N = 1298; // dataset size;
 
 void printArray1 (std::string name, double *a, int printLim, bool inRow = true) {
 	if (inRow) {
@@ -381,15 +381,178 @@ int main() {
 
 	// Bi-CGSTAB solve:
 	// NOTE: This has to be done inside the main() function
-	auto startBi_CGSTAB = std::chrono::high_resolution_clock::now();
-	Bi_CGSTAB_solve(dG, q, alphas); // you had a good life, but I can't use you! :'(
-	auto endBi_CGSTAB = std::chrono::high_resolution_clock::now();
+	// auto startBi_CGSTAB = std::chrono::high_resolution_clock::now();
+	// Bi_CGSTAB_solve(dG, q, alphas); // you had a good life, but I can't use you! :'(
+	// auto endBi_CGSTAB = std::chrono::high_resolution_clock::now();
 
 	// =========================================================================================
 	// ========== Bi-CGSTAB Implementation inside main() =======================================
+	auto startBi_CGSTAB = std::chrono::high_resolution_clock::now();
 
+	// ctrl. constants
+	int maxIter = 100;
+	double tol = 1e-5;
 
+	// iter vectors
+	double *x_curr = new double[N];
+	double *x_next = new double[N];
 
+	double *r_curr = new double[N];
+	double *r_next = new double[N];
+
+	double *rp0 = new double[N];
+
+	double *p_curr = new double[N];
+	double *p_next = new double[N];
+
+	double *s = new double[N];
+
+	double *tmp = new double[N];
+	double *tmp1 = new double[N];
+
+	// iter scalars
+	double alpha, beta, omega;
+
+	// x0 = (1000,1000,...,1000)
+	for (int i = 0; i < N; i++) x_curr[i] = 1000.;
+	// r0 = b - A x0
+	// choose rp0 such that <r0, rp0> != 0
+	// p0 = r0
+	for (int i = 0; i < N; i++) {
+		r_curr[i] = q[i];
+		for (int j = 0; j < N; j++) {
+			r_curr[i] -= dG[i][j] * x_curr[j];
+		}
+		rp0[i] = r_curr[i] + 100;
+		p_curr[i] = r_curr[i];
+	}
+	std::cout << "==================================================" << std::endl;
+	std::cout << "----------- Initializing Bi-CGSTAB Method --------" << std::endl;
+	printArray2("systemMatrix", dG, 4);
+	printArray1("systemRhs", q, 5);
+	printArray1("x0", x_curr, 2);
+	printArray1("r0", r_curr, 5);
+
+	std::cout << "--------------------------------------------------" << std::endl;
+	std::cout << "------------ Launching iterations ----------------" << std::endl;
+	// begin iterations
+	for (int k = 0; k < maxIter; k++) {
+		std::cout << "::: iter : " << k << std::endl;
+
+		// alpha[k] = <r[k], rp0> / <Ap[k], rp0>
+
+		double num = 0.; double den = 0.;
+		for (int i = 0; i < N; i++) {
+			tmp[i] = 0.;
+			for (int j = 0; j < N; j++) {
+				tmp[i] += dG[i][j] * p_curr[j];
+			}
+			num += r_curr[i] * rp0[i];
+			den += tmp[i] * rp0[i];
+		}
+		alpha = num / den;
+
+		// s[k] = r[k] - alpha[k] * A p[k]
+
+		for (int i = 0; i < N; i++) {
+			s[i] = r_curr[i] - alpha * tmp[i];
+		}
+
+		double norm = vectorNorm(s);
+		std::cout << "||s|| = " << norm << std::endl;
+		if (norm < tol) {
+			// x[k + 1] = x[k] + alpha[k] * p[k]
+
+			for (int i = 0; i < N; i++) {
+				x_next[i] = x_curr[i] + alpha * p_curr[i];
+			}
+
+			std::cout << "||s|| < tol = " << tol << ", exiting iterations" << std::endl;
+			break;
+		}
+
+		// omega[k] = <A s[k], s[k]> / <A s[k], A s[k]>
+
+		num = 0; den = 0;
+		for (int i = 0; i < N; i++) {
+			tmp[i] = 0;
+			for (int j = 0; j < N; j++) {
+				tmp[i] += dG[i][j] * s[j];
+			}
+			num += tmp[i] * s[i];
+			den += tmp[i] * tmp[i];
+		}
+		omega = num / den;
+
+		// x[k + 1] = x[k] + alpha[k] * p[k] + omega[k] * s[k]
+
+		for (int i = 0; i < N; i++) {
+			x_next[i] = x_curr[i] + alpha * p_curr[i] + omega * s[i];
+		}
+
+		// r[k + 1] = s[k] - omega[k] * A s[k]
+
+		for (int i = 0; i < N; i++) {
+			r_next[i] = s[i] - omega * tmp[i];
+		}
+
+		norm = vectorNorm(r_next);
+		std::cout << "||r[k + 1]|| = " << norm << std::endl;
+		if (norm < tol) {
+			std::cout << "||r[k + 1]|| < tol = " << tol << ", exiting iterations" << std::endl;
+			break;
+		}
+
+		// beta[k] = (alpha[k] / omega[k]) * <r[k + 1], rp0> / <r[k], rp0>
+		
+		num = 0; den = 0;
+		for (int i = 0; i < N; i++) {
+			num += r_next[i] * rp0[i];
+			den += r_curr[i] * rp0[i];
+		}
+
+		beta = (alpha / omega) * num / den;
+
+		// p[k + 1] = r[k + 1] + beta[k] * (p[k] - omega[k] * A p[k])
+
+		for (int i = 0; i < N; i++) {
+			tmp[i] = 0;
+			for (int j = 0; j < N; j++) {
+				tmp[i] += dG[i][j] * p_curr[j];
+			}
+			p_next[i] = r_next[i] + beta * (p_curr[i] - omega * tmp[i]);
+		}
+
+		norm = fabs(vectorDot(r_next, rp0));
+		std::cout << "|< r[k + 1], rp0 >| = " << norm << std::endl;
+		if (norm < tol) {
+			// rp0 = r[k + 1]; p[k + 1] = r[k + 1]
+			
+			for (int i = 0; i < N; i++) {
+				rp0[i] = r_next[i]; p_next[i] = r_next[i];
+			}
+		}
+		// current = next
+
+		for (int i = 0; i < N; i++) {
+			x_curr[i] = x_next[i];
+			r_curr[i] = r_next[i];
+			p_curr[i] = p_next[i];
+		}
+
+		std::cout << "===> finishing iter " << k << std::endl;
+	}
+
+	// result: x = x_next
+	for (int i = 0; i < N; i++) alphas[i] = x_next[i];
+
+	// clean up
+	delete[] x_curr; delete[] x_next;
+	delete[] r_curr; delete[] r_next;
+	delete[] p_curr; delete[] p_next;
+	delete[] s; delete[] tmp; delete[] tmp1;
+
+	auto endBi_CGSTAB = std::chrono::high_resolution_clock::now();
 	// ========================= Solution done ===============================================
 
 	// print solution
@@ -407,7 +570,7 @@ int main() {
 	// ================================ Summary ============================================
 
 	printf("\n============================================================================\n");
-	printf("======================== Program summary =====================================\n");
+	printf("======================= Program summary =====================================\n");
 	std::cout << "data size = " << N << std::endl;
 	std::chrono::duration<double> elapsedTotal = (endMMult - startLoad);
 	std::cout << "total runtime :    " << elapsedTotal.count() << " s" << std::endl;
